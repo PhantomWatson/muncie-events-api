2581820090fad186fa21bec52ca3217eb5bfd441
Graham Watson <graham@phantomwatson.com>
2017-11-27 20:55:10 -0500

    Automatically hash passwords

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -60,4 +61,15 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+
+    /**
+     * Automatically hashes password
+     *
+     * @param string $password Password
+     * @return bool|string
+     */
+    protected function _setPassword($password)
+    {
+        return (new DefaultPasswordHasher)->hash($password);
+    }
 }
