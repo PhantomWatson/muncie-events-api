eaf786dc12155b333f11f71b062fecbd142b50fb
Graham Watson <graham@phantomwatson.com>
2017-12-01 20:34:53 -0500

    Add prefix routing for /v1

--- config/routes.php
+++ config/routes.php
@@ -71,6 +71,10 @@ Router::scope('/', function (RouteBuilder $routes) {
     $routes->fallbacks(DashedRoute::class);
 });
 
+Router::prefix('v1', function (RouteBuilder $routes) {
+    $routes->fallbacks(DashedRoute::class);
+});
+
 /**
  * Load all plugin routes. See the Plugin documentation on
  * how to customize the loading of plugin routes.
