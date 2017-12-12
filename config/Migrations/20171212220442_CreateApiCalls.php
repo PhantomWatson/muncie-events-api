ca721613b9bbc694c28ca9428fa1479746fceb81
Graham Watson <graham@phantomwatson.com>
2017-12-12 17:06:18 -0500

    Add CreateApiCalls migration

---
+++ config/Migrations/20171212220442_CreateApiCalls.php
@@ -0,0 +1,33 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateApiCalls extends AbstractMigration
+{
+
+    public function up()
+    {
+        $this->table('api_calls')
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('url', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->dropTable('api_calls');
+    }
+}
+
