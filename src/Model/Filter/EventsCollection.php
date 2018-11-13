bcbf3c9635e4d1ef2b4c3fc5f4cf5c1038a6763d
Graham Watson <graham@phantomwatson.com>
2018-11-13 16:09:29 -0500

    Add EventsCollection filter for use in /events/search endpoint

---
+++ src/Model/Filter/EventsCollection.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Model\Filter;
+
+use Search\Model\Filter\FilterCollection;
+
+class EventsCollection extends FilterCollection
+{
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->add('q', 'Search.Like', [
+            'field' => ['title', 'description', 'location'],
+
+            // Automatically add wildcards before and after a search term
+            'before' => true,
+            'after' => true
+        ]);
+    }
+}
