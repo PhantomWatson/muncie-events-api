f3d01a7000262a80f2b1eaa08cfb2a04ab7cb9b4
Graham Watson <graham@phantomwatson.com>
2018-11-21 17:00:48 -0500

    Add testIndexSuccess()

---
+++ tests/TestCase/Controller/V1/CategoriesControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Test\Fixture\CategoriesFixture;
+use App\Test\TestCase\ApplicationTest;
+use Cake\Utility\Hash;
+
+/**
+ * CategoriesControllerTest class
+ */
+class CategoriesControllerTest extends ApplicationTest
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.api_calls',
+        'app.categories',
+        'app.event_series',
+        'app.events',
+        'app.events_images',
+        'app.events_tags',
+        'app.images',
+        'app.tags',
+        'app.users'
+    ];
+
+    /**
+     * Tests that /v1/categories returns the correct results
+     *
+     * @return void
+     * @throws \PHPUnit\Exception
+     */
+    public function testIndexSuccess()
+    {
+        $this->get([
+            'prefix' => 'v1',
+            'controller' => 'Categories',
+            'action' => 'index',
+            '?' => ['apikey' => $this->getApiKey()]
+        ]);
+        $this->assertResponseOk();
+
+        // Test that all categories are included
+        $categories = (new CategoriesFixture())->records;
+        $categoryIds = Hash::extract($categories, '{n}.id');
+        sort($categoryIds);
+        $response = (array)json_decode($this->_response->getBody());
+        $responseCategories = $response['data'];
+        $responseCategoryIds = Hash::extract($responseCategories, '{n}.id');
+        sort($responseCategoryIds);
+        $this->assertEquals(
+            $categoryIds,
+            $responseCategoryIds,
+            sprintf(
+                'Event categories expected: %s; Returned: %s',
+                print_r($categoryIds, true),
+                print_r($responseCategoryIds, true)
+            )
+        );
+
+        // Test that event counts are non-zero
+        $eventCounts = Hash::extract($responseCategories, '{n}.attributes.upcomingEventCount');
+        $this->assertNotContains(
+            0,
+            $eventCounts,
+            'One or more categories has a zero value for upcomingEventCount'
+        );
+    }
+}
