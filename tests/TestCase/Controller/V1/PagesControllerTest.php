587be79eb3df052bd95862c911641bd1de5912dd
Graham Watson <graham@phantomwatson.com>
2018-12-10 14:46:42 -0500

    Test /pages/about endpoint

---
+++ tests/TestCase/Controller/V1/PagesControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Test\TestCase\Controller\V1;
+
+use App\Test\TestCase\ApplicationTest;
+
+/**
+ * PagesControllerTest class
+ */
+class PagesControllerTest extends ApplicationTest
+{
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.api_calls',
+        'app.users'
+    ];
+
+    /**
+     * Sets up this set of tests
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->configRequest([
+            'environment' => ['HTTPS' => 'on']
+        ]);
+    }
+
+    /**
+     * Tests /pages/about endpoint
+     *
+     * @return void
+     * @throws \PHPUnit\Exception
+     */
+    public function testAbout()
+    {
+        $this->get([
+            'prefix' => 'v1',
+            'controller' => 'Pages',
+            'action' => 'about',
+            '?' => ['apikey' => $this->getApiKey()]
+        ]);
+        $this->assertResponseOk();
+        $response = (array)json_decode($this->_response->getBody());
+        $page = $response['data']->attributes;
+        $this->assertTrue(isset($page->title), 'About page is missing a title');
+        $this->assertTrue(isset($page->body), 'About page is missing a body');
+    }
+}
