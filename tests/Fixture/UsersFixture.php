5a97a8eb9016cbdad394f8238019158ee92ca2cf
Graham Watson <graham@phantomwatson.com>
2019-04-01 12:00:34 -0400

    Un-associate first user from mailing list

--- tests/Fixture/UsersFixture.php
+++ tests/Fixture/UsersFixture.php
@@ -56,7 +56,7 @@ class UsersFixture extends TestFixture
             'bio' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
             'email' => 'user1@example.com',
             'password' => '',
-            'mailing_list_id' => 1,
+            'mailing_list_id' => null,
             'facebook_id' => 1,
             'api_key' => 'KOsc08Hf1cOLUpbt1NHwoTwA2BnCIUSZ',
             'token' => 'xl0iV04yK5gtabGX4v9es6eWR93BxkTg',
