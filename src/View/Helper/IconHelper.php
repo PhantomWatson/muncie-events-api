e5276de603a3eaf6bf0d90a19ce9fef222795fac
Graham Watson <graham@phantomwatson.com>
2019-05-08 19:25:26 -0400

    Add IconHelper

---
+++ src/View/Helper/IconHelper.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\View\Helper;
+
+use Cake\Core\Configure;
+use Cake\View\Helper;
+
+class IconHelper extends Helper
+{
+    /**
+     * Outputs a category icon
+     *
+     * @param string $categoryName Category name
+     * @param string|null $mode Current output mode; set to 'email' if in an email
+     * @return string
+     */
+    public static function category($categoryName, $mode = null)
+    {
+        if ($mode == 'email') {
+            $base = Configure::read('fullBaseUrl');
+            $dir = $base . 'img/icons/categories/';
+            $filename = 'meicon_' . strtolower(str_replace(' ', '_', $categoryName)) . '_32x32.png';
+
+            return sprintf(
+                '<img src="%s%s" title="%s" class="category" alt="%s" />',
+                $dir,
+                $filename,
+                $categoryName,
+                $categoryName
+            );
+        }
+
+        $class = 'icon icon-' . strtolower(str_replace(' ', '-', $categoryName));
+
+        return sprintf('<i class="%s" title="%s"></i>', $class, $categoryName);
+    }
+}
