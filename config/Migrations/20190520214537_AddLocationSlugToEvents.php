c385c80e325a0fbc0b47fde9a5aea7aae6e87e30
Graham Watson <graham@phantomwatson.com>
2019-05-20 17:55:40 -0400

    Add AddLocationSlugToEvents migration

---
+++ config/Migrations/20190520214537_AddLocationSlugToEvents.php
@@ -0,0 +1,26 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddLocationSlugToEvents extends AbstractMigration
+{
+    /**
+     * Adds the location_slug column to the events table
+     *
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('events');
+
+        $table
+            ->addColumn(
+                'location_slug',
+                'string',
+                [
+                    'after' => 'location_details',
+                    'limit' => 50,
+                    'null' => false
+                ])
+            ->update();
+    }
+}
