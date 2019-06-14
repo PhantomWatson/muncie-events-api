d227f3e9e2006b651bef6a7311f80ac466ced235
Graham Watson <graham@phantomwatson.com>
2019-06-14 19:36:48 -0400

    Add TagHelper

---
+++ src/View/Helper/TagHelper.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\View\Helper;
+
+use App\Model\Entity\Event;
+use Cake\Datasource\ResultSetInterface;
+use Cake\ORM\TableRegistry;
+use Cake\View\Helper;
+use Cake\View\Helper\HtmlHelper;
+
+/**
+ * @property HtmlHelper $Html
+ */
+class TagHelper extends Helper
+{
+    public $helpers = ['Html'];
+
+    /**
+     * Buffers JS that pre-selects tags in the event form
+     *
+     * @param Event $event Event entity
+     * @return void
+     */
+    private function preselectTags($event)
+    {
+        $selectedTags = [];
+        if (is_array($event->tags)) {
+            foreach ($event->tags as $tag) {
+                $selectedTags[] = [
+                    'id' => $tag->id,
+                    'name' => $tag->name
+                ];
+            }
+        }
+        $this->Html->scriptBlock(
+            sprintf('TagManager.preselectTags(%s);', json_encode($selectedTags)),
+            ['block' => true]
+        );
+    }
+
+    /**
+     * Buffers JS for the tag menu
+     *
+     * @param string $containerSelector The CSS selector of the 'available tags' container
+     * @param Event $event The event associated with the current page
+     * @return void
+     */
+    public function setup($containerSelector, $event)
+    {
+        $this->preselectTags($event);
+        $this->setAvailableTags($containerSelector);
+        $this->Html->scriptBlock(
+            '
+                $(\'#new_tag_rules_toggler\').click(function(event) {
+                    event.preventDefault();
+                    $(\'#new_tag_rules\').slideToggle(200);
+                });
+            ',
+            ['block' => true]
+        );
+    }
+
+    /**
+     * Buffers JS for creating a menu of selectable tags
+     *
+     * @param string $containerSelector The CSS selector of the 'available tags' container
+     * @return void
+     */
+    private function setAvailableTags($containerSelector)
+    {
+        $tagsTable = TableRegistry::getTableLocator()->get('Tags');
+        $results = $tagsTable->find('threaded')
+            ->where(['listed' => true])
+            ->orderAsc('name')
+            ->all();
+        $availableTags = $this->availableTagsToArray($results);
+
+        $this->Html->scriptBlock(
+            sprintf(
+                'TagManager.createTagList(%s, $(%s));',
+                json_encode($availableTags),
+                json_encode($containerSelector)
+            ),
+            ['block' => true]
+        );
+    }
+
+    /**
+     * Takes a threaded resultset of available tags and returns a nested array
+     *
+     * @param ResultSetInterface $tags ResultSet of tags
+     * @return array
+     */
+    private function availableTagsToArray($tags)
+    {
+        $retval = [];
+        foreach ($tags as $tag) {
+            $retval[] = [
+                'id' => $tag->id,
+                'name' => $tag->name,
+                'selectable' => (bool)$tag->selectable,
+                'children' => $this->availableTagsToArray($tag->children)
+            ];
+        }
+
+        return $retval;
+    }
+}
