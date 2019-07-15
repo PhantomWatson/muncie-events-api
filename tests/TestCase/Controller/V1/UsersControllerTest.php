8e7046fb282040b4315d965446ab0d3a20b783d5
Graham Watson <graham@phantomwatson.com>
2019-07-15 16:24:28 -0400

    Stop escaping slashes in URL
    This seemed to be necessary previously, but now this test won't pass
    if slashes are escaped. Maybe some dependency code changed?

--- tests/TestCase/Controller/V1/UsersControllerTest.php
+++ tests/TestCase/Controller/V1/UsersControllerTest.php
@@ -390,10 +390,6 @@ class UsersControllerTest extends ApplicationTest
         $this->assertMailSentFrom(Configure::read('automailer_address'));
         $this->assertMailSentTo($user->email);
         $resetUrl = 'https://muncieevents.com/reset_password/' . $user->id . '/' . $user->getResetPasswordHash();
-
-        // Slashes need to be escaped because $resetUrl is used as pattern in preg_match("/$resetUrl/")
-        $resetUrl = str_replace('/', '\/', $resetUrl);
-
         $this->assertMailContains($resetUrl);
     }
 
