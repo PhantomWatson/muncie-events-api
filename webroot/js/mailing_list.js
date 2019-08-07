b2b12e3afdfe2c76007a3c7e8e0b7d3d2a4d1b82
Graham Watson <graham@phantomwatson.com>
2019-08-07 16:49:17 -0400

    Add mailing_list.js

---
+++ webroot/js/mailing_list.js
@@ -0,0 +1,39 @@
+var mailingList = {
+    init: function () {
+        this.toggleEventTypeOptions();
+        this.toggleBasicOptions();
+        $('.frequency_options').change(function (event) {
+            mailingList.toggleFrequencyOptions();
+        });
+        $('.category_options').change(function (event) {
+            mailingList.toggleEventTypeOptions();
+        });
+        $('.settings_options').change(function (event) {
+            mailingList.toggleBasicOptions();
+        });
+    },
+
+    toggleFrequencyOptions: function () {
+        if ($('#frequency-custom').is(':checked')) {
+            $('#custom_frequency_options').slideDown(300);
+        } else {
+            $('#custom_frequency_options').slideUp(300);
+        }
+    },
+
+    toggleEventTypeOptions: function () {
+        if ($('#event-categories-custom').is(':checked')) {
+            $('#custom_event_type_options').slideDown(300);
+        } else {
+            $('#custom_event_type_options').slideUp(300);
+        }
+    },
+
+    toggleBasicOptions: function () {
+        if ($('#settings-custom').is(':checked')) {
+            $('#custom_options').slideDown(300);
+        } else {
+            $('#custom_options').slideUp(300);
+        }
+    }
+};
