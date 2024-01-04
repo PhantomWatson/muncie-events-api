4dc627186c45ed6479f764d8cab51280f6f70f72
Graham Watson <graham@phantomwatson.com>
2024-01-03 23:37:06 -0500

    Stop extending Cors plugin

--- src/Error/AppExceptionRenderer.php
+++ src/Error/AppExceptionRenderer.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Error;
 
+use Cake\Error\ExceptionRenderer;
 use Cake\Http\Response;
-use Cors\Error\AppExceptionRenderer as CorsExceptionRenderer;
 
-class AppExceptionRenderer extends CorsExceptionRenderer
+class AppExceptionRenderer extends ExceptionRenderer
 {
     /**
      * Renders the response for the exception.
