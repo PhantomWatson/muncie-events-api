773a5dd276a838566d0c2d10c7841627b9392ac8
Graham Watson <graham@phantomwatson.com>
2026-01-04 15:00:50 -0500

    Put action buttons in dropdown

--- templates/element/Events/actions.php
+++ templates/element/Events/actions.php
@@ -20,80 +20,96 @@ if ($widget) {
 }
 ?>
 <div class="actions">
-    <div class="export_options_container">
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                title="Export to another calendar">
+            <i class="fas fa-cloud-download-alt"></i>
+            Export
+        </button>
+        <div class="<?= $dropdownMenuClasses ?>" aria-labelledby="dropdownMenuButton">
+            <a href="<?= CalendarHelper::getGoogleCalendarUrl($event) ?>" title="Add to Google Calendar"
+               class="dropdown-item">
+                Google
+            </a>
+            <?= $this->Html->link(
+                'Outlook / iCalendar (.ics)',
+                [
+                    'plugin' => false,
+                    'prefix' => false,
+                    'controller' => 'Events',
+                    'action' => 'view',
+                    'id' => $event->id,
+                    '_ext' => 'ics',
+
+                ],
+                [
+                    'title' => 'Download iCalendar (.ics) file to import into Microsoft Outlook or another calendar application',
+                    'class' => 'dropdown-item',
+                ]
+            ) ?>
+        </div>
+    </div>
+    <?php if ($canEdit): ?>
         <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                    title="Export to another calendar">
-                <i class="fas fa-cloud-download-alt"></i>
-                Export
+            <button
+                class="btn btn-secondary dropdown-toggle btn-sm"
+                type="button"
+                id="manageEventDropdown"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                title="Manage this event"
+            >
+                Manage
             </button>
-            <div class="<?= $dropdownMenuClasses ?>" aria-labelledby="dropdownMenuButton">
-                <a href="<?= CalendarHelper::getGoogleCalendarUrl($event) ?>" title="Add to Google Calendar"
-                   class="dropdown-item">
-                    Google
-                </a>
+            <div class="<?= $dropdownMenuClasses ?>" aria-labelledby="manageEventDropdown">
+                <?php if ($isAdmin && !$isApproved): ?>
+                    <?= $this->Html->link(
+                        'Approve',
+                        [
+                            'plugin' => false,
+                            'prefix' => false,
+                            'controller' => 'Events',
+                            'action' => 'approve',
+                            'id' => $event->id,
+                        ],
+                        [
+                            'escape' => false,
+                            'class' => 'dropdown-item',
+                        ]
+                    ) ?>
+                <?php endif; ?>
                 <?= $this->Html->link(
-                    'Outlook / iCalendar (.ics)',
+                    'Edit',
                     [
                         'plugin' => false,
                         'prefix' => false,
                         'controller' => 'Events',
-                        'action' => 'view',
+                        'action' => 'edit',
                         'id' => $event->id,
-                        '_ext' => 'ics',
-
                     ],
                     [
-                        'title' => 'Download iCalendar (.ics) file to import into Microsoft Outlook or another calendar application',
                         'class' => 'dropdown-item',
+                        'escape' => false,
+                    ]
+                ) ?>
+                <?= $this->Form->postLink(
+                    'Delete',
+                    [
+                        'plugin' => false,
+                        'prefix' => false,
+                        'controller' => 'Events',
+                        'action' => 'delete',
+                        'id' => $event->id,
+                    ],
+                    [
+                        'class' => 'dropdown-item',
+                        'confirm' => 'Are you sure you want to delete this event?',
+                        'escape' => false,
                     ]
                 ) ?>
             </div>
         </div>
-    </div>
-    <?php if ($isAdmin && !$isApproved): ?>
-        <?= $this->Html->link(
-            $this->Html->image('/img/icons/tick.png', ['alt' => 'Approve this event']) . 'Approve',
-            [
-                'plugin' => false,
-                'prefix' => false,
-                'controller' => 'Events',
-                'action' => 'approve',
-                'id' => $event->id,
-            ],
-            ['escape' => false]
-        ) ?>
-    <?php endif; ?>
-    <?php if ($canEdit): ?>
-        <?= $this->Html->link(
-            'Edit',
-            [
-                'plugin' => false,
-                'prefix' => false,
-                'controller' => 'Events',
-                'action' => 'edit',
-                'id' => $event->id,
-            ],
-            [
-                'class' => 'btn btn-sm btn-secondary',
-                'escape' => false,
-            ]
-        ) ?>
-        <?= $this->Form->postLink(
-            'Delete',
-            [
-                'plugin' => false,
-                'prefix' => false,
-                'controller' => 'Events',
-                'action' => 'delete',
-                'id' => $event->id,
-            ],
-            [
-                'class' => 'btn btn-sm btn-secondary',
-                'confirm' => 'Are you sure you want to delete this event?',
-                'escape' => false,
-            ]
-        ) ?>
     <?php endif; ?>
 </div>
