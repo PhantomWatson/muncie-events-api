5aab12d0b0aae7f8f809121de9bef4949e5ce797
Graham Watson <graham@phantomwatson.com>
2026-05-15 05:46:16 +0000

    Fix use of nonexistent method setTime()

--- templates/Events/ics/feed.php
+++ templates/Events/ics/feed.php
@@ -9,18 +9,18 @@ use App\Model\Entity\Event;
 use Sabre\VObject\Component\VCalendar;
 
 $vcalendar = new VCalendar();
-$from = 0; // now
 
 // Determine the end of the range of dates needed for the VTIMEZONE definition
-$to = 0;
+$to = new DateTime();
 foreach ($events as $event) {
-    $timestamp = $event->date->setTime(23, 59, 59)->toUnixString();
-    if ($timestamp > $to) {
-        $to = $timestamp;
+    $dateTime = new DateTime($event->date->format('Y-m-d'));
+    if ($dateTime > $to) {
+        $to = $dateTime;
     }
 }
+$to->setTime(23, 59, 59);
 
-$vcalendar = Event::addVtimezone($vcalendar, Event::TIMEZONE, $from, $to);
+$vcalendar = Event::addVtimezone($vcalendar, Event::TIMEZONE, 0, $to->getTimestamp());
 
 foreach ($events as $event) {
     $vcalendar->add('VEVENT', VEvent::getVevent($event));
