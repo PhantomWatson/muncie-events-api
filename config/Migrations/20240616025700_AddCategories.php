06b210d4998f8e72acce54283bfc3a70e96da536
Graham Watson <graham@phantomwatson.com>
2026-06-22 01:48:57 +0000

    Modernize migrations

--- config/Migrations/20240616025700_AddCategories.php
+++ config/Migrations/20240616025700_AddCategories.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
 
-use Migrations\AbstractMigration;
+use Migrations\BaseMigration;
 
-class AddCategories extends AbstractMigration
+class AddCategories extends BaseMigration
 {
     public function up(): void
     {
