89767806426c7fd758df4cd12627fc29573b770d
Graham Watson <graham@phantomwatson.com>
2026-08-29 07:58:43 +0000

    Update API prefix used in API responses

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -57,7 +57,7 @@ class ApiController extends Controller
         EventManager::instance()->on($apiCallsListener);
 
         $this->viewBuilder()->setClassName('JsonApi.JsonApi');
-        $this->set('_url', Router::url('/v1', true));
+        $this->set('_url', Router::url('/api/v1', true));
         $this->set('_entities', [
             'Category',
             'Event',
