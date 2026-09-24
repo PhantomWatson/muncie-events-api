f6d225e3ae4ec85faff8c9d1f2f137c7c9b40b7f
Graham Watson <graham@phantomwatson.com>
2026-09-24 22:01:52 +0000

    Add test for rejecting malicious styles

--- tests/TestCase/Controller/WidgetsControllerTest.php
+++ tests/TestCase/Controller/WidgetsControllerTest.php
@@ -18,7 +18,16 @@ class WidgetsControllerTest extends ApplicationTest
      *
      * @var array
      */
-    public array $fixtures = [];
+    public array $fixtures = [
+        'app.Categories',
+        'app.EventSeries',
+        'app.Events',
+        'app.EventsImages',
+        'app.EventsTags',
+        'app.Images',
+        'app.Tags',
+        'app.Users',
+    ];
 
     /**
      * Test index method
@@ -33,5 +42,63 @@ class WidgetsControllerTest extends ApplicationTest
             'action' => 'index',
         ]);
         $this->assertResponseOk();
+    }
+
+    /**
+     * Tests that malicious style values are not reflected into the month widget
+     *
+     * @return void
+     * @throws \PHPUnit\Exception
+     */
+    public function testMonthRejectsMaliciousStyles()
+    {
+        $payload = 'red;}</style><svg/onload=alert(1)>';
+        $this->get([
+            'controller' => 'Widgets',
+            'action' => 'month',
+            '?' => [
+                'backgroundColorAlt' => '#fffdc6',
+                'fontSize' => $payload,
+                'textColorLink' => $payload,
+            ],
+        ]);
+        $this->assertResponseOk();
+        $this->assertResponseNotContains('<svg/onload');
+        $this->assertResponseContains('background-color: #fffdc6;');
+    }
+
+    /**
+     * Tests that a malicious location filter is escaped in the feed widget
+     *
+     * @return void
+     * @throws \PHPUnit\Exception
+     */
+    public function testFeedEscapesLocationFilter()
+    {
+        $this->get([
+            'controller' => 'Widgets',
+            'action' => 'feed',
+            '?' => ['location' => '<script>alert(1)</script>'],
+        ]);
+        $this->assertResponseOk();
+        $this->assertResponseNotContains('<script>alert(1)</script>');
+        $this->assertResponseContains('&lt;script&gt;alert(1)&lt;/script&gt;');
+    }
+
+    /**
+     * Tests that a malicious border color is not reflected into the customization page's iframe styles
+     *
+     * @return void
+     * @throws \PHPUnit\Exception
+     */
+    public function testCustomizeMonthRejectsMaliciousBorderColor()
+    {
+        $this->get([
+            'controller' => 'Widgets',
+            'action' => 'customizeMonth',
+            '?' => ['borderColorDark' => 'red" onload="alert(1)'],
+        ]);
+        $this->assertResponseOk();
+        $this->assertResponseNotContains('onload="alert(1)');
     }
 }
